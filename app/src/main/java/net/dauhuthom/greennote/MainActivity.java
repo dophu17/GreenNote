63f9afb6a1fd524b180eaf6bb5a2c7b91861eb35
dophu17 <dophu17@gmail.com>
2016-11-07 16:57:31 +0700

    dsddd

--- app/src/main/java/net/dauhuthom/greennote/MainActivity.java
+++ app/src/main/java/net/dauhuthom/greennote/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         addControls();
         addEvents();
-        readData();
+//        readData();
     }
 
     private void addEvents() {
