dcbf836c3f99c6fb135bfb1ce76143e893dd5284
dophu17 <dophu17@gmail.com>
2016-12-09 10:50:24 +0700

    udpate setting

--- app/src/main/java/net/dauhuthom/greennote/NoteDBHelper.java
+++ app/src/main/java/net/dauhuthom/greennote/NoteDBHelper.java
@@ -80,13 +80,13 @@ public class NoteDBHelper extends DBhelper {
 
     public Cursor getAllJoinThisMonth() {
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
-        Cursor cursor =  sqLiteDatabase.rawQuery("SELECT notes.*, services.name as name FROM " + TABLE_NOTE_NAME + " LEFT JOIN " + TABLE_SERVICE_NAME + " ON notes.service_id = services.id WHERE date BETWEEN date('now', 'start of month') AND date('now', 'start of month', '+1 month', '-1 days')", null);
+        Cursor cursor =  sqLiteDatabase.rawQuery("SELECT notes.*, services.name as name FROM " + TABLE_NOTE_NAME + " LEFT JOIN " + TABLE_SERVICE_NAME + " ON notes.service_id = services.id WHERE date BETWEEN date('now', 'start of month') AND date('now', 'start of month', '+1 month', '-1 days') ORDER BY " + COLUMN_NOTE_DATE + " ASC", null);
         return cursor;
     }
 
     public Cursor getAllJoinLastMonth() {
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
-        Cursor cursor =  sqLiteDatabase.rawQuery("SELECT notes.*, services.name as name FROM " + TABLE_NOTE_NAME + " LEFT JOIN " + TABLE_SERVICE_NAME + " ON notes.service_id = services.id WHERE date BETWEEN date('now', 'start of month', '-1 month') AND date('now', 'start of month', '-1 days')", null);
+        Cursor cursor =  sqLiteDatabase.rawQuery("SELECT notes.*, services.name as name FROM " + TABLE_NOTE_NAME + " LEFT JOIN " + TABLE_SERVICE_NAME + " ON notes.service_id = services.id WHERE date BETWEEN date('now', 'start of month', '-1 month') AND date('now', 'start of month', '-1 days') ORDER BY " + COLUMN_NOTE_DATE + " ASC", null);
         return cursor;
     }
 
